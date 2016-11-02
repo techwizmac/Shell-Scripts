900540c779b768a2ac084d1dfa8388c4a42160f9
techwizmac <maxin.flame@gmail.com>
2016-11-02 10:17:40 -0400

    CMS-Ver-Extract

--- CMS-Version-Extraction/extract.sh
+++ CMS-Version-Extraction/extract.sh
@@ -1,61 +1,55 @@
 #!/bin/sh 
 #
 # ===================================================================
-# CMS information extraction script using an existing list of sites
+# CMS information extraction
 # Author: techwizmac, 2016
 # ===================================================================
 #
 set -x  # debugging starts
-# Empty files prior to processing
-for item in $(ls results-*);
- do truncate -s 0 $item;
-done
+
+# Empty result file in case anything was left from prior run
 truncate -s 0 CMS-Report
-# Generate header information from each website list using WhatWeb utility
+
+# Extract header info from each website-list using WhatWeb utility and
+# dump CMS version info only on temp file 'cms-results'
 for item in $(ls ./WEBSITES/);
   do whatweb -v -i ./WEBSITES/$item >>results-$item;
+  cat results-$item|grep -e "\s*\+String \+: \+.\+$item" -e "\s*\+IP" -e report >>CMS-Report
 done
-# Extract CMS version info and dump results on a temp file
-echo "Wordpress extract..."
-cat results-wordpress|grep -e "\s*\+String \+: \+.\+WordPress" -e "\s*\+IP" -e report >>cms-results
-echo "Drupal extract..."
-cat results-drupal|grep -e "\s*\+String \+: \+.\+Drupal" -e "\s*\+IP" -e report >>cms-results
-echo "Joomla extract..."
-cat results-joomla|grep -e "\s*\+String \+: \+.\+Joomla" -e "\s*\+IP" -e report >>cms-results
-echo "Plone extract..."
-cat results-plone|grep -e "\s*\+String \+: \+.\+Zope" -e "\s*\+IP" -e report >>cms-results
-echo "Other extract..."
-cat results-other|grep -e "\s*\+String \+: \+.\+Other" -e "\s*\+IP" -e report >>cms-results
+
+#Beautify text
+sed -i 's/WhatWeb //' CMS-Report
+sed -i 's/IP        : /IP:/' CMS-Report
+sed -i 's/String       : /MetaGenerator: /' CMS-Report
+sed -i ':a;N;$!ba;s/\n/  /' CMS-Report
+sed -i 's/report for /\n/' CMS-Report
+sed -i 's/Powered by Visual Composer - drag and drop page builder for WordPress.,//' CMS-Report
+sed -i 's/- Open Source Content Management//' CMS-Report
+
 # Change text colour for outdated versions
-cat cms-results|sed ''/3.5.1/s//`printf "\033[31m3.5.1\033[0m"`/'' > cms-results1
-cat cms-results1|sed ''/3.8.3/s//`printf "\033[31m3.8.3\033[0m"`/'' > cms-results2
-cat cms-results2|sed ''/3.9.1/s//`printf "\033[31m3.9.1\033[0m"`/'' > cms-results3
-cat cms-results3|sed ''/3.2.1/s//`printf "\033[31m3.2.1\033[0m"`/'' > cms-results4
-cat cms-results4|sed ''/4.2.1/s//`printf "\033[31m4.2.1\033[0m"`/'' > cms-results5
-cat cms-results5|sed ''/4.2.4/s//`printf "\033[31m4.2.4\033[0m"`/'' > cms-results6
-cat cms-results6|sed ''/4.5.1/s//`printf "\033[31m4.5.1\033[0m"`/'' > cms-results7
-cat cms-results7|sed ''/3.8.4/s//`printf "\033[31m3.8.4\033[0m"`/'' > cms-results8
-cat cms-results8|sed ''/4.3.1/s//`printf "\033[31m4.3.1\033[0m"`/'' > cms-results9
-cat cms-results9|sed ''/4.1.1/s//`printf "\033[31m4.1.1\033[0m"`/'' > cms-results10
-cat cms-results10|sed ''/4.4.4/s//`printf "\033[31m4.1.1\033[0m"`/'' > cms-results11
-cat cms-results11|sed ''/4.3.5/s//`printf "\033[31m4.3.5\033[0m"`/'' > cms-results12
-cat cms-results12|sed ''/3.5.2/s//`printf "\033[31m3.5.2\033[0m"`/'' > cms-results13
-cat cms-results13|sed ''/4.4.5/s//`printf "\033[31m4.4.5\033[0m"`/'' > cms-results14
-cat cms-results14|sed ''/4.4.2/s//`printf "\033[31m4.4.2\033[0m"`/'' > cms-results15
-cat cms-results15|sed ''/3.1.2/s//`printf "\033[31m3.1.2\033[0m"`/'' > cms-results-temp
-#cat cms-results11|sed ''/Joomla! 1.5/s//`printf "\033[31mJoomla! 1.5\033[0m"`/'' > cms-results12
-sed 's/WhatWeb //g' cms-results-temp >>cms-results-temp1
-sed 's/IP        : /IP:/g' cms-results-temp1 >>cms-results-temp2
-sed 's/String       : /MetaGenerator:/g' cms-results-temp2 >>cms-results-temp3
-sed ':a;N;$!ba;s/\n/  /g' cms-results-temp3 >>cms-results-temp4
-sed 's/report for /\n/g' cms-results-temp4 >>cms-results-temp5
-sed 's/Powered by Visual Composer - drag and drop page builder for WordPress.,//g' cms-results-temp5 >>cms-results-temp6
-sed 's/- Open Source Content Management//g' cms-results-temp6 >>CMS-Report
-#Remove intermediate files
-for item in $(ls cms-results*);
+sed -i ''/'WordPress 3.5.1'/s//`printf "\033[31mWordPress_3.5.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 3.8.3'/s//`printf "\033[31mWordPress_3.8.3\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 3.9.1'/s//`printf "\033[31mWordPress_3.9.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 3.2.1'/s//`printf "\033[31mWordPress_3.2.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.2.1'/s//`printf "\033[31mWordPress_4.2.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.2.4'/s//`printf "\033[31mWordPress_4.2.4\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.5.1'/s//`printf "\033[31mWordPress_4.5.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 3.8.4'/s//`printf "\033[31mWordPress_3.8.4\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.3.1'/s//`printf "\033[31mWordPress_4.3.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.1.1'/s//`printf "\033[31mWordPress_4.1.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.4.4'/s//`printf "\033[31mWordPress_4.1.1\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.3.5'/s//`printf "\033[31mWordPress_4.3.5\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.3.6'/s//`printf "\033[31mWordPress_4.3.6\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 3.5.2'/s//`printf "\033[31mWordPress_3.5.2\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.4.5'/s//`printf "\033[31mWordPress_4.4.5\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 4.4.2'/s//`printf "\033[31mWordPress_4.4.2\033[0m"`/'' CMS-Report
+sed -i ''/'WordPress 3.1.2'/s//`printf "\033[31mWordPress_3.1.2\033[0m"`/'' CMS-Report
+sed -i ''/'Joomla! 1.5'/s//`printf "\033[31mJoomla!_1.5\033[0m"`/'' CMS-Report
+
+# Remove temporary files
+for item in $(ls results-*);
  do rm $item;
 done
 set +x  # debugging ends
-
 # Done
 

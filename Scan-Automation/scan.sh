b762334648b88f8e3e40cbd72c77095e20a673ef
techwizmac <maxin.flame@gmail.com>
2017-10-17 11:58:57 -0400

    Oct-17-2017

--- Scan-Automation/scan.sh
+++ Scan-Automation/scan.sh
@@ -17,17 +17,19 @@ Menu(){
     NUMBER=`echo "\033[33m"` #yellow
     ENTER_LINE=`echo "\033[33m"` #yellow
     echo "${MENU}**********************************${NORMAL}"
-    echo "${MENU}${NUMBER}1 ${MENU}--> HTTPIE  ${NORMAL}"
+    echo "${MENU}${NUMBER}1 ${MENU}--> HTTPIE  0.8.0${NORMAL}"
     echo "${MENU}${NUMBER}2 ${MENU}--> WHATWEB ${NORMAL}"
-    echo "${MENU}${NUMBER}3 ${MENU}--> HPING 80 & 443 ${NORMAL}"
-    echo "${MENU}${NUMBER}4 ${MENU}--> NMAP ${NORMAL}"
-    echo "${MENU}${NUMBER}5 ${MENU}--> NIKTO ${NORMAL}"
-    echo "${MENU}${NUMBER}6 ${MENU}--> SSH-AUDIT ${NORMAL}"
-    echo "${MENU}${NUMBER}7 ${MENU}--> RDP-SEC-CHECK ${NORMAL}"
-    echo "${MENU}${NUMBER}8 ${MENU}--> TESTSSL ${NORMAL}"
-    echo "${MENU}${NUMBER}9 ${MENU}--> RUN ALL TESTS ${NORMAL}"
+    echo "${MENU}${NUMBER}3 ${MENU}--> WAFW00F 0.9.4 ${NORMAL}"
+    echo "${MENU}${NUMBER}4 ${MENU}--> HPING 3 (80 & 443) ${NORMAL}"
+    echo "${MENU}${NUMBER}5 ${MENU}--> NMAP 7.40${NORMAL}"
+    echo "${MENU}${NUMBER}6 ${MENU}--> NIKTO 2.6${NORMAL}"
+    echo "${MENU}${NUMBER}7 ${MENU}--> SSH-AUDIT 1.7.0${NORMAL}"
+    echo "${MENU}${NUMBER}8 ${MENU}--> RDP-SEC-CHECK 0.8 Beta${NORMAL}"
+    echo "${MENU}${NUMBER}9 ${MENU}--> TESTSSL 2.8 RC1${NORMAL}"
+    echo "${MENU}${NUMBER}10 ${MENU}--> SSL SCAN 1.8.2${NORMAL}"
+    echo "${MENU}${NUMBER}11 ${MENU}--> OPEN SSL 1.0.1f${NORMAL}"
+    echo "${MENU}${NUMBER}12 ${MENU}--> RUN ALL TESTS ${NORMAL}"
     echo "${MENU}${NUMBER}0 ${MENU}--> Exit ${NORMAL}"
-
     echo "${MENU}**********************************${NORMAL}"
     echo "${ENTER_LINE}Select a menu option and enter"
     read opt
@@ -45,7 +47,7 @@ while [ opt != '' ]
  do
  if [[ $opt = ""]]; then 
    exit;
- else
+else
  case $opt in
 
 #  HTTPIE
@@ -66,8 +68,16 @@ while [ opt != '' ]
 Menu;
 ;;
 
-# HPING 80,443
  3) clear;
+   wafw00f -v -a --findall $(cat host) >>Scan-Report
+   echo '\n' >>Scan-Report
+   echo '\n' >>Scan-Report
+   clear
+Menu;
+;;
+
+# HPING 80,443
+ 4) clear;
    sudo hping3 -8 80,443 -S $(cat host) -V >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
@@ -76,7 +86,7 @@ Menu;
 ;;
 
 #  NMAP
- 4) clear;
+ 5) clear;
    sudo nmap -A -T4 -sSV --version-intensity 9 --script banner $(cat host) -Pn -O -vvv -dd >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
@@ -85,7 +95,7 @@ Menu;
 ;;
 
 # NIKTO-216
- 5) clear;
+ 6) clear;
    sudo nmap -p 80,443 $(cat host) -oG -|~/Documents/TOOLS/Web-Tools/Nikto216/program/nikto.pl -h - -C all >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
@@ -94,7 +104,7 @@ Menu;
 ;;
 
 # SSH-AUDIT
- 6) clear;
+ 7) clear;
    ssh-audit -v $(cat host) >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
@@ -103,7 +113,7 @@ Menu;
 ;;
 
 # RDP-SEC-CHECK
- 7) clear;
+ 8) clear;
    rdpcheck $(cat host):3389 >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
@@ -111,8 +121,8 @@ Menu;
 Menu;
 ;;
 
-# RDP-SEC-CHECK
- 8) clear;
+# TESTSSL
+ 9) clear;
    testssl $(cat host) >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
@@ -120,13 +130,33 @@ Menu;
 Menu;
 ;;
 
+#  SSLSCAN
+ 10) clear;
+   sslscan $(cat host) >>Scan-Report
+   echo '\n' >>Scan-Report
+   echo '\n' >>Scan-Report
+# clear
+Menu;
+;;
+
+# OPENSSL
+ 11) clear;
+   openssl s_client -connect $(cat host):443 -showcerts >>Scan-Report-openssl
+   echo '\n' >>Scan-Report
+   echo '\n' >>Scan-Report
+# clear
+Menu;
+;;
 
 # RUN ALL TESTS
- 9) clear;
+ 12) clear;
    http --pretty all --verbose --output Scan-Report $(cat host)
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
    whatweb -a3 -v -i $(cat host) >>Scan-Report
+   echo '\n' >>Scan-Report
+   echo '\n' >>Scan-Report
+   wafw00f -v -a --findall $(cat host) >>Scan-Report
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
    sudo hping3 -8 80,443 -S $(cat host) -V >>Scan-Report
@@ -145,6 +175,12 @@ Menu;
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
    testssl $(cat host) >>Scan-Report
+   echo '\n' >>Scan-Report
+   echo '\n' >>Scan-Report
+   sslscan $(cat host) >>Scan-Report
+   echo '\n' >>Scan-Report
+   echo '\n' >>Scan-Report
+   openssl s_client -connect $(cat host):443 -showcerts >>Scan-Report-openssl
    echo '\n' >>Scan-Report
    echo '\n' >>Scan-Report
 # clear
